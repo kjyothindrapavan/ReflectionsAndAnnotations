8fa3514c3093f499dd5e2018a1c778e4d9a4ef42
Jyothindra Pavan <kjyothindrapavan@gmail.com>
2021-08-09 17:47:02 +0530

    remove module-info

---
+++ src/com/basicsstrong/practice/Launcher.java
@@ -0,0 +1,14 @@
+package com.basicsstrong.practice;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+public class Launcher {
+
+	public static void main(String[] args) throws SQLException {
+		// TODO Auto-generated method stub
+		Server.main();
+
+	}
+
+}
